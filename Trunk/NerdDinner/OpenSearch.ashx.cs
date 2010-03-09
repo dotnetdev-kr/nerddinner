6d2538b76f1fa077c4edcab95d72d5ba7ebec1b8
jongalloway <none@none.com>
2010-03-09 17:19:17 +0000

    Added DinnersNearMe services - needs refactoring as it duplicates code in SearchController. Added OpenSearchToolkit and DDay.iCal, although these aren't fully implemented yet.

---
+++ Trunk/NerdDinner/OpenSearch.ashx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using OpenSearchToolkit;
+using NerdDinner.Models;
+
+namespace NerdDinner
+{
+    /// <summary>
+    /// Summary description for OpenSearch
+    /// </summary>
+    public class OpenSearch : OpenSearchHandler
+    {
+        protected override Description Description
+        {
+            get
+            {
+                return new Description
+                {
+                    DisplayName = "Nerd Dinner",
+                    LongDescription = "Nerd Dinner - Organizing the world's nerds and helping them eat in packs",
+                    SearchPathTemplate = "/#where={0}",
+                    IconPath = "~/favicon.ico"
+                };
+            }
+        }
+
+        protected override IEnumerable<SearchResult> GetResults(string term)
+        {
+            var dinnerRepository = new DinnerRepository();
+            var dinners = dinnerRepository.FindAllDinners().Where(d => d.Title.Contains(term)).AsEnumerable();
+
+            return from dinner in dinners
+                   select new
+                       SearchResult
+                       {
+                           Description = dinner.Description,
+                           Title = dinner.Title,
+                           Path = "/" + dinner.DinnerID
+                       };
+        }
+
+        protected override IEnumerable<SearchSuggestion> GetSuggestions(string term)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override bool SupportsSuggestions
+        {
+            get { return false; }
+        }
+    }
+}
--- Trunk/NerdDinner/Services.svc.cs
+++ Trunk/NerdDinner/Services.svc.cs
@@ -6,10 +6,13 @@ using System.Linq;
 using System.ServiceModel.Web;
 using System.Web;
 using NerdDinner.Models;
+using System.Xml.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
 
 namespace NerdDinner
 {
-    [System.ServiceModel.ServiceBehavior(IncludeExceptionDetailInFaults = true)]
+    [ServiceBehavior(IncludeExceptionDetailInFaults = true, InstanceContextMode = InstanceContextMode.Single, ConcurrencyMode = ConcurrencyMode.Single)]
     public class Services : DataService<NerdDinnerEntities>
     {
         IDinnerRepository dinnerRepository;
@@ -31,6 +34,7 @@ namespace NerdDinner
             // We're exposing both Dinners and RSVPs for read
             config.SetEntitySetAccessRule("Dinners", EntitySetRights.AllRead);
             config.SetEntitySetAccessRule("RSVPs", EntitySetRights.AllRead);
+            config.SetServiceOperationAccessRule("DinnersNearMe", ServiceOperationRights.AllRead);
             config.SetServiceOperationAccessRule("*", ServiceOperationRights.All);
             config.DataServiceBehavior.MaxProtocolVersion = DataServiceProtocolVersion.V2;
             #if DEBUG
@@ -42,6 +46,50 @@ namespace NerdDinner
         public IQueryable<Dinner> FindUpcomingDinners()
         {
             return dinnerRepository.FindUpcomingDinners();
+        }
+
+        // http://localhost:60848/Services.svc/DinnersNearMe?placeOrZip='12345'
+        [WebGet]
+        public IQueryable<Dinner> DinnersNearMe(string placeOrZip)
+        {
+            if (String.IsNullOrEmpty(placeOrZip)) return null; ;
+
+            string url = "http://ws.geonames.org/postalCodeSearch?{0}={1}&maxRows=1&style=SHORT";
+            url = String.Format(url, IsNumeric(placeOrZip) ? "postalcode" : "placename", placeOrZip);
+
+            var result = HttpContext.Current.Cache[placeOrZip] as XDocument;
+            if (result == null)
+            {
+                result = XDocument.Load(url);
+                HttpContext.Current.Cache.Insert(placeOrZip, result, null, DateTime.Now.AddDays(1), System.Web.Caching.Cache.NoSlidingExpiration);
+            }
+
+            var LatLong = (from x in result.Descendants("code")
+                           select new
+                           {
+                               Lat = (float)x.Element("lat"),
+                               Long = (float)x.Element("lng")
+                           }).First();
+
+            var dinners = dinnerRepository.
+                            FindByLocation(LatLong.Lat, LatLong.Long).
+                            OrderByDescending(p => p.EventDate);
+            return dinners;
+        }
+
+        // IsNumeric Function
+        private bool IsNumeric(object Expression)
+        {
+            // Variable to collect the Return value of the TryParse method.
+            bool isNum;
+
+            // Define variable to collect out parameter of the TryParse method. If the conversion fails, the out parameter is zero.
+            double retNum;
+
+            // The TryParse method converts a string in a specified style and culture-specific format to its double-precision floating point number equivalent.
+            // The TryParse method does not generate an exception if the conversion fails. If the conversion passes, True is returned. If it does not, False is returned.
+            isNum = Double.TryParse(Convert.ToString(Expression), System.Globalization.NumberStyles.Any, System.Globalization.NumberFormatInfo.InvariantInfo, out retNum);
+            return isNum;
         }
     }
 }
