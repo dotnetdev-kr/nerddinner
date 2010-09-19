096654354ca63539871367c3dcbbc794b65b6b41
JasonHaley <none@none.com>
2010-09-19 18:49:05 +0000

    Added version of NerdDinner that uses NHaml for the views except: Account/LogOn, Shared/LogOnContent, Shared/Site.Master, EditorTemplates and all Mobile views.  Project uses the default view engine for the views listed above due to dependency on Page.

---
+++ Branches/Playground/VS2010-MVC2-NHaml/NerdDinner/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NerdDinner.Controllers {
+
+    [HandleErrorWithELMAH]
+    public class HomeController : Controller {
+        private const string XrdsType = "application/xrds+xml";
+        
+        public ActionResult Index() {
+            // Enables RP Discovery, which avoids warnings from OpenID providers like Yahoo during login.
+            // Some Providers ask for a specific accept-type, which we can optimize for here.
+            if (Request.AcceptTypes != null && Array.IndexOf(Request.AcceptTypes, XrdsType) >= 0) {
+                return View("Xrds");
+            }
+            // Other Providers don't say they're performing RP discovery, so always include an HTTP header to help them.
+            Response.AppendHeader("X-XRDS-Location", Url.Action("Xrds"));
+
+            return View();
+        }
+
+        public ActionResult About() {
+            return View();
+        }
+
+        public ActionResult PrivacyPolicy()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Provides an RP discovery document to OpenID 2.0 Providers that are performing a security check on this RP.
+        /// </summary>
+        public ActionResult Xrds() {
+            Response.ContentType = "application/xrds+xml";
+            
+            var result = View("Xrds");
+            result.MasterName = "empty";
+
+            return result;
+        }
+
+        public ActionResult Test()
+        {
+            return View();
+        }
+    }
+}
