80ee8862eade82181fc00e412df8972af3b67850
pmourfield <none@none.com>
2012-07-02 18:58:44 +0000

    added a missing file

---
+++ Trunk/NerdDinner/Controllers/ViewSwitcherController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NerdDinner.Controllers
+{
+    public class ViewSwitcherController : Controller
+    {
+        public RedirectResult SwitchView(bool mobile, string returnUrl)
+        {
+            if (Request.Browser.IsMobileDevice == mobile)
+            {
+                HttpContext.ClearOverriddenBrowser();
+            }
+            else
+            {
+                HttpContext.SetOverriddenBrowser(mobile ? BrowserOverride.Mobile : BrowserOverride.Desktop);
+            }
+            return Redirect(returnUrl);
+        }
+    }
+}
