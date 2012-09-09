760f02e7cc3e7e0dd267a4bd480d048048269280
Peter Mourfield <pete@mourfield.com>
2012-09-09 10:24:03 -0400

    some more view updates
    some more view updates

--- mvc4/NerdDinner/App_Start/AuthConfig.cs
+++ mvc4/NerdDinner/App_Start/AuthConfig.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using Microsoft.Web.WebPages.OAuth;
 using NerdDinner.Models;
+using System.Web.Configuration;
 
 namespace NerdDinner
 {
@@ -14,17 +15,32 @@ namespace NerdDinner
             // To let users of this site log in using their accounts from other sites such as Microsoft, Facebook, and Twitter,
             // you must update this site. For more information visit http://go.microsoft.com/fwlink/?LinkID=252166
 
-            //OAuthWebSecurity.RegisterMicrosoftClient(
-            //    clientId: "",
-            //    clientSecret: "");
+            var microsoftClientId = WebConfigurationManager.AppSettings["microsoftClientId"];
+            var microsoftClientSecret = WebConfigurationManager.AppSettings["microsoftClientSecret"];
+            if (!string.IsNullOrEmpty(microsoftClientId) && !string.IsNullOrEmpty(microsoftClientSecret))
+            {
+                OAuthWebSecurity.RegisterMicrosoftClient(
+                    clientId: microsoftClientId,
+                    clientSecret: microsoftClientSecret);
+            }
 
-            //OAuthWebSecurity.RegisterTwitterClient(
-            //    consumerKey: "",
-            //    consumerSecret: "");
+            var twitterConsumerKey = WebConfigurationManager.AppSettings["twitterConsumerKey"];
+            var twitterConsumerSecret = WebConfigurationManager.AppSettings["twitterConsumerSecret"];
+            if (!string.IsNullOrEmpty(twitterConsumerKey) && !string.IsNullOrEmpty(twitterConsumerSecret))
+            {
+                OAuthWebSecurity.RegisterTwitterClient(
+                    consumerKey: twitterConsumerKey,
+                    consumerSecret: twitterConsumerSecret);
+            }
 
-            //OAuthWebSecurity.RegisterFacebookClient(
-            //    appId: "",
-            //    appSecret: "");
+            var facebookAppId = WebConfigurationManager.AppSettings["facebookAppId"];
+            var facebookAppSecret = WebConfigurationManager.AppSettings["facebookAppSecret"];
+            if (!string.IsNullOrEmpty(facebookAppId) && !string.IsNullOrEmpty(facebookAppSecret))
+            {
+                OAuthWebSecurity.RegisterFacebookClient(
+                    appId: facebookAppId,
+                    appSecret: facebookAppSecret);
+            }
 
             OAuthWebSecurity.RegisterGoogleClient();
         }
