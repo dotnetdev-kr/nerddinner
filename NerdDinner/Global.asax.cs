b8bcebd305a3533a3125abd7bae7a15052e43251
Peter Mourfield <pete@mourfield.com>
2013-09-21 19:50:41 -0400

    fixed role creation method

--- NerdDinner/Global.asax.cs
+++ NerdDinner/Global.asax.cs
@@ -1,6 +1,12 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using NerdDinner.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
@@ -14,14 +20,9 @@ namespace NerdDinner
     // visit http://go.microsoft.com/fwlink/?LinkId=301868
     public class MvcApplication : System.Web.HttpApplication
     {
-        protected void Application_Start()
+        protected async void Application_Start()
         {
             System.Data.Entity.Database.SetInitializer(new System.Data.Entity.DropCreateDatabaseIfModelChanges<NerdDinner.Models.NerdDinnerContext>());
-
-            if (!Roles.Provider.RoleExists("Admin"))
-            {
-                Roles.Provider.CreateRole("Admin");
-            }
 
             AreaRegistration.RegisterAllAreas();
 
@@ -29,6 +30,23 @@ namespace NerdDinner
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+           await SetupApplicationRoles();
+        }
+
+        async Task<bool> SetupApplicationRoles()
+        {
+            AuthenticationIdentityManager IdentityManager = new AuthenticationIdentityManager(new IdentityStore(new ApplicationDbContext()));
+
+            bool roleExists = await IdentityManager.Roles.RoleExistsAsync("Admin", CancellationToken.None);
+            if (roleExists == false)
+            {
+                var role = new Role("Admin");
+                IdentityResult result = await IdentityManager.Roles.CreateRoleAsync(role);
+                if (result.Success == false)
+                    return false;
+            }
+            return true;
         }
     }
 }
