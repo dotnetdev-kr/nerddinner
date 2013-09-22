20096c63241f6e1119e2df5a7abfc7e3946e1640
Peter Mourfield <pete@mourfield.com>
2013-09-21 20:17:18 -0400

    update bootstrap template

--- NerdDinner/App_Start/BundleConfig.cs
+++ NerdDinner/App_Start/BundleConfig.cs
@@ -20,12 +20,12 @@ namespace NerdDinner
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css",
-                      "~/Content/bootstrap-responsive.css"));
+                      "~/Content/site.css"));
         }
     }
 }
