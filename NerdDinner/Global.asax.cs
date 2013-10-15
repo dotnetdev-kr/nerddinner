421ca042dc65918ac5e3e31cd88d50ba1eb32b74
Peter Mourfield <pete@mourfield.com>
2013-10-14 21:34:52 -0400

    added first round of test data generation

--- NerdDinner/Global.asax.cs
+++ NerdDinner/Global.asax.cs
@@ -1,18 +1,12 @@
 ﻿using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
-using NerdDinner.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
-using System.Web.Security;
 
 namespace NerdDinner
 {
@@ -22,7 +16,7 @@ namespace NerdDinner
     {
         protected async void Application_Start()
         {
-            System.Data.Entity.Database.SetInitializer(new System.Data.Entity.DropCreateDatabaseIfModelChanges<NerdDinner.Models.NerdDinnerContext>());
+            System.Data.Entity.Database.SetInitializer(new NerdDinner.Models.NerdDinnerContext.DropCreateIfChangeInitializer());
 
             AreaRegistration.RegisterAllAreas();
 
--- NerdDinner/Models/NerdDinnerContext.cs
+++ NerdDinner/Models/NerdDinnerContext.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
-using System.Linq;
-using System.Web;
+using System.Data.Entity.Spatial;
 
 namespace NerdDinner.Models
 {
@@ -17,11 +16,40 @@ namespace NerdDinner.Models
         // 
         // System.Data.Entity.Database.SetInitializer(new System.Data.Entity.DropCreateDatabaseIfModelChanges<NerdDinner.Models.NerdDinnerContext>());
 
-        public NerdDinnerContext() : base("name=NerdDinnerContext")
+        public NerdDinnerContext()
+            : base("name=NerdDinnerContext")
         {
         }
 
         public System.Data.Entity.DbSet<NerdDinner.Models.Dinner> Dinners { get; set; }
+        public System.Data.Entity.DbSet<NerdDinner.Models.RSVP> RSVPs { get; set; }
 
+        public class DropCreateIfChangeInitializer : DropCreateDatabaseIfModelChanges<NerdDinnerContext>
+        {
+            protected override void Seed(NerdDinnerContext context)
+            {
+                var testDinner1 = new Dinner()
+                {
+                    DinnerID = 1,
+                    Title = "Dinner at the White House",
+                    EventDate = DateTime.Now.AddDays(5),
+                    Description = "Dinner at the White House",
+                    HostedBy = "The President",
+                    ContactPhone = "(202) 456-1111",
+                    Address = "1600 Pennsylvania Ave Nw Washington, DC 20500",
+                    Country = "USA",
+                    Location = DbGeography.PointFromText(string.Format("POINT({0} {1})", -77.036545, 38.897096), DbGeography.DefaultCoordinateSystemId),
+                    RSVPs = new List<RSVP>()
+                };
+                testDinner1.RSVPs.Add(new RSVP() { RsvpID = 1, DinnerID = 1, AttendeeName = "@shanselman" });
+                testDinner1.RSVPs.Add(new RSVP() { RsvpID = 2, DinnerID = 1, AttendeeName = "@pmourfield" });
+
+                context.Dinners.Add(testDinner1);
+
+                context.SaveChanges();
+
+                base.Seed(context);
+            }
+        }
     }
 }
