3b6318a8ac29e6d95678055efabbc65d9e5d2d13
tawalke <tara.walker@microsoft.com>
2013-10-20 11:04:28 -0400

    Updated ViewModel Structure

--- NerdDinner.Learning/NerdDinner.Core.Mobile/ViewModels/FirstViewModel.cs
+++ NerdDinner.Learning/NerdDinner.Core.Mobile/ViewModels/FirstViewModel.cs
@@ -2,14 +2,14 @@ using Cirrious.MvvmCross.ViewModels;
 
 namespace NerdDinner.Core.Mobile.ViewModels
 {
-    public class FirstViewModel 
+	public class FirstViewModel 
 		: MvxViewModel
-    {
+	{
 		private string _hello = "Hello MvvmCross";
-        public string Hello
+		public string Hello
 		{ 
 			get { return _hello; }
 			set { _hello = value; RaisePropertyChanged(() => Hello); }
 		}
-    }
+	}
 }
